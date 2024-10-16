52b4daeaf15c811c0b2922b2c05bb37cd2c9ade1
interns <interns@caratred.com>
2024-10-16 19:07:46 +0530

    apis

--- ezyformsfrontend/src/shared/apiurls.js
+++ ezyformsfrontend/src/shared/apiurls.js
@@ -8,12 +8,17 @@ export const fileDomain = "";
 export const doctypes = {
   // poschecks: "User",
   users: "User",
+  dept: "Ezy Departments",
 };
 
 export const apis = {
   resource: domain + `/resource/`,
   uploadfile: domain + `/method/upload_file`,
+  savedata:
+    domain +
+    `/method/ezy_forms.ezy_forms.doctype.ezy_form_definitions.ezy_form_definitions.add_dynamic_doctype`,
 };
+
 // export const tokens = {
 //   Apikey: "1812146a6f090f1",
 //   secretkey: "7a5e80cad93659f",
