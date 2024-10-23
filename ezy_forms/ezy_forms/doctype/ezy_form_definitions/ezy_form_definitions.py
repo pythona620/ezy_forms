33855e64fc5b17659725199a62ff237e6fffcde8
maniram.y <maniram.y@caratred.com>
2024-10-23 17:01:31 +0530

    enqueued and separated add fields and delete field and removed delete doc

--- ezy_forms/ezy_forms/doctype/ezy_form_definitions/ezy_form_definitions.py
+++ ezy_forms/ezy_forms/doctype/ezy_form_definitions/ezy_form_definitions.py
@@ -9,12 +9,48 @@ import subprocess
 import os
 from ast import literal_eval
 import pandas as pd
+from frappe.utils.background_jobs import enqueue
 
 class EzyFormDefinitions(Document):
 	pass
 
 @frappe.whitelist()
 def add_dynamic_doctype(owner_of_the_form:str,business_unit:str,form_category:str,form_name:str,accessible_departments:str,form_short_name:str,fields:list[dict]):
+	enqueue(
+		enqueued_add_dynamic_doctype,
+		owner_of_the_form=owner_of_the_form,
+		business_unit=business_unit,
+		form_category=form_category,
+		form_name=form_name,
+		accessible_departments=accessible_departments,
+		form_short_name=form_short_name,
+		fields=fields,
+		now=True,
+		is_async=True, 
+		queue="short")
+
+@frappe.whitelist()
+def add_customized_fields_for_dynamic_doc(fields:list[dict],doctype:str):
+	enqueue(
+		enqueued_add_customized_fields_for_dynamic_doc,
+		fields=fields,
+		doctype=doctype,
+		now=True,
+		is_async=True, 
+		queue="short")
+
+@frappe.whitelist()
+def deleting_customized_field_from_custom_dynamic_doc(doctype,field):
+	enqueue(
+		enqueued_deleting_customized_field_from_custom_dynamic_doc,
+		doctype=doctype,
+		field=field,
+		now=True,
+		is_async=True, 
+		queue="short")
+
+@frappe.whitelist()
+def enqueued_add_dynamic_doctype(owner_of_the_form:str,business_unit:str,form_category:str,form_name:str,accessible_departments:str,form_short_name:str,fields:list[dict]):
 	""" Owner_of_the_form should come from Departments Doctype in Select Field."""
 	"""Adding DocTypes dynamically, giving Perms for the doctype and creating a default section-break field for DocType"""
 	try:
@@ -23,53 +59,50 @@ def add_dynamic_doctype(owner_of_the_form:str,business_unit:str,form_category:st
 		if isinstance(fields,str):
 			fields = literal_eval(fields)
 		if frappe.db.exists("Ezy Form Definitions",doctype):return {"success":False,"message":f"Already '{doctype}' exists. Please rename the form."}
-		if not frappe.db.exists("DocType",doctype):
-			doc = frappe.new_doc("DocType")
-			doc.name = doctype
-			doc.creation = frappe_now()
-			doc.modified = frappe_now()
-			doc.modified_by = frappe.session.user
-			doc.module = "User Forms"
-			doc.app = "ezy_forms"
-			doc.custom = 1
-			doc.insert(ignore_permissions=True)
-			frappe.db.commit()
-			doc.reload()
-			custom_docperm = frappe.new_doc("Custom DocPerm")
-			custom_docperm.parent = doctype
-			custom_docperm.read = 1
-			custom_docperm.create = 1
-			custom_docperm.delete = 1
-			custom_docperm.select = 1
-			custom_docperm.write = 1
-			custom_docperm.role = "System Manager"
-			custom_docperm.insert(ignore_permissions=True)
-			frappe.db.commit()
-			custom_docperm.reload()
-			form_defs = frappe.new_doc("Ezy Form Definitions")
-			form_defs.form_category = form_category
-			form_defs.accessible_departments = accessible_departments
-			form_defs.form_name = form_name
-			form_defs.form_short_name = form_short_name
-			form_defs.owner_of_the_form = owner_of_the_form
-			form_defs.active = 1
-			form_defs.business_unit = business_unit
-			form_defs.count = 0
-			form_defs.insert(ignore_permissions=True).save()
-			form_defs.reload()
-			frappe.db.commit()
-			"""need to migrate in this step itself as DocType is created need to update in DB."""
-			bench_migrating_from_code()
+		doc = frappe.new_doc("DocType")
+		doc.name = doctype
+		doc.creation = frappe_now()
+		doc.modified = frappe_now()
+		doc.modified_by = frappe.session.user
+		doc.module = "User Forms"
+		doc.app = "ezy_forms"
+		doc.custom = 1
+		doc.insert(ignore_permissions=True)
+		frappe.db.commit()
+		doc.reload()
+		custom_docperm = frappe.new_doc("Custom DocPerm")
+		custom_docperm.parent = doctype
+		custom_docperm.read = 1
+		custom_docperm.create = 1
+		custom_docperm.delete = 1
+		custom_docperm.select = 1
+		custom_docperm.write = 1
+		custom_docperm.role = "System Manager"
+		custom_docperm.insert(ignore_permissions=True)
+		frappe.db.commit()
+		custom_docperm.reload()
+		form_defs = frappe.new_doc("Ezy Form Definitions")
+		form_defs.form_category = form_category
+		form_defs.accessible_departments = accessible_departments
+		form_defs.form_name = form_name
+		form_defs.form_short_name = form_short_name
+		form_defs.owner_of_the_form = owner_of_the_form
+		form_defs.active = 1
+		form_defs.business_unit = business_unit
+		form_defs.count = 0
+		form_defs.insert(ignore_permissions=True).save()
+		form_defs.reload()
+		frappe.db.commit()
+		"""need to migrate in this step itself as DocType is created need to update in DB."""
+		bench_migrating_from_code()
 
-			"""need to add perms in this step itself for system manager to access the DocType"""
-			activating_perms(doctype=doctype)
-			document_to_reload = frappe.get_doc("DocType",doctype)
-			document_to_reload.reload()
+		"""need to add perms in this step itself for system manager to access the DocType"""
+		activating_perms(doctype=doctype)
+		document_to_reload = frappe.get_doc("DocType",doctype)
+		document_to_reload.reload()
 
-			"""need to migrate in this step itself because of adding permissions in the below level"""
-			bench_migrating_from_code()
-		if len(fields)>0:
-			add_customized_fields_for_dynamic_doc(fields=fields,doctype=doctype)
+		"""need to migrate in this step itself because of adding permissions in the below level"""
+		bench_migrating_from_code()
 	except Exception as e:
 		exc_type, exc_obj, exc_tb = sys.exc_info()
 		if "list index out of range" in str(e):
@@ -81,7 +114,7 @@ def add_dynamic_doctype(owner_of_the_form:str,business_unit:str,form_category:st
 		return {"success": False, "message": str(e)}
 
 @frappe.whitelist()
-def add_customized_fields_for_dynamic_doc(fields:list[dict],doctype:str):
+def enqueued_add_customized_fields_for_dynamic_doc(fields:list[dict],doctype:str):
 	try:
 		if isinstance(fields,str):fields = literal_eval(fields)
 		if not len(fields)>0:return {"success":False,"message":"Pass Fields for storing."}
@@ -136,7 +169,7 @@ def add_customized_fields_for_dynamic_doc(fields:list[dict],doctype:str):
 		return {"success": False, "message": str(e)}
 
 @frappe.whitelist()
-def deleting_customized_field_from_custom_dynamic_doc(doctype,field):
+def enqueued_deleting_customized_field_from_custom_dynamic_doc(doctype,field):
 	try:
 		if not frappe.db.exists("DocType",doctype): return {"success":False,"message":f"The Form mentioned doesnot exists - '{doctype}'"}
 		frappe.db.delete("DocField",{"parent":doctype,"fieldname":field})
