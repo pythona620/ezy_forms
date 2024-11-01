4bf69a4be4789b84bd38036d90b268e43eb77c23
maniram.y <maniram.y@caratred.com>
2024-11-01 15:18:00 +0530

    roles also added in wf roles

--- ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.py
+++ ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.py
@@ -12,9 +12,19 @@ class EzyEmployee(Document):
 				adding_role_doc.role_name = self.designation
 				adding_role_doc.insert(ignore_permissions=True)
 				frappe.db.commit()
+			if not frappe.db.exists("WF Roles",{"role":self.designation}):
+				adding_role_doc = frappe.new_doc("WF Roles")
+				adding_role_doc.role_name = self.designation
+				adding_role_doc.insert(ignore_permissions=True)
+				frappe.db.commit()
 		if self.reporting_designation:
 			if not frappe.db.exists("Role",{"role_name":self.reporting_designation}):
 				adding_role_doc = frappe.new_doc("Role")
+				adding_role_doc.role_name = self.reporting_designation
+				adding_role_doc.insert(ignore_permissions=True)
+				frappe.db.commit()
+			if not frappe.db.exists("WF Roles",{"role":self.reporting_designation}):
+				adding_role_doc = frappe.new_doc("WF Roles")
 				adding_role_doc.role_name = self.reporting_designation
 				adding_role_doc.insert(ignore_permissions=True)
 				frappe.db.commit()
