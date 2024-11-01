150cde45c00877390336a2735a69381ef1bd9b2f
maniram.y <maniram.y@caratred.com>
2024-11-01 12:47:40 +0530

    done roles adding when adding employee designation and reportee designation

--- ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.py
+++ ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2024, bharath and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class EzyEmployee(Document):
-	pass
+	def after_insert(self):
+		if self.designation:
+			if not frappe.db.exists("Role",{"role_name":self.designation}):
+				adding_role_doc = frappe.new_doc("Role")
+				adding_role_doc.role_name = self.designation
+				adding_role_doc.insert(ignore_permissions=True)
+				frappe.db.commit()
+		if self.reporting_designation:
+			if not frappe.db.exists("Role",{"role_name":self.reporting_designation}):
+				adding_role_doc = frappe.new_doc("Role")
+				adding_role_doc.role_name = self.reporting_designation
+				adding_role_doc.insert(ignore_permissions=True)
+				frappe.db.commit()
