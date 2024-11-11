24fd08573af52faae48e351264199a0854b6f0a8
maniram.y <maniram.y@caratred.com>
2024-11-11 11:38:57 +0530

    done employee_role_based perms

--- ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.py
+++ ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.py
@@ -68,7 +68,8 @@ class EzyEmployee(Document):
 def role_based_accessible_requests(role:str,business_unit:str):
 	try:
 		list_of_roadmaps = frappe.db.get_all("WF Requestors",{"parent":["like",f"%{business_unit}%"],"requestor":role},pluck="parent")
-		return {"success":True,"list_of_roadmaps":list_of_roadmaps}
+		list_of_short_names = frappe.db.get_all("WF Roadmap",{"name":["in",list_of_roadmaps]},pluck="document_type")
+		return {"success":True,"list_of_roadmaps":list_of_short_names}
 	except Exception as e:
 		exc_type, exc_obj, exc_tb = sys.exc_info()
 		frappe.log_error("Error in Role based Roadmaps.",
