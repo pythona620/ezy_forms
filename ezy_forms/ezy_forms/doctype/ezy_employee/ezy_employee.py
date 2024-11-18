d90153bfb991b6f8f346a1d8f4816c8aa01d7f83
maniram.y <maniram.y@caratred.com>
2024-11-18 18:40:03 +0530

    bench migrate given and doctype definitions added

--- ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.py
+++ ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.py
@@ -4,7 +4,7 @@
 import frappe
 from frappe.model.document import Document
 import sys
-from ezy_forms.ezy_forms.doctype.ezy_form_definitions.ezy_form_definitions import activating_perms
+from ezy_forms.ezy_forms.doctype.ezy_form_definitions.ezy_form_definitions import activating_perms, bench_migrating_from_code
 
 class EzyEmployee(Document):
 	def after_insert(self):
@@ -28,9 +28,10 @@ class EzyEmployee(Document):
 				adding_role_doc.role = self.designation
 				adding_role_doc.insert(ignore_permissions=True)
 				frappe.db.commit()
-			for doc_name in ["Ezy Employee","WF Workflow Requests","WF Roadmap","Ezy Business Unit","WF Roles"]:
+			for doc_name in ["Ezy Employee","WF Workflow Requests","WF Roadmap","Ezy Business Unit","WF Roles","Ezy Form Definitions"]:
 				if not frappe.db.exists("DocPerm",{"parent":doc_name,"parenttype":"DocType","role":self.designation,"parentfield":"permissions"}):
 					activating_perms(doc_name,self.designation)
+			bench_migrating_from_code()
 		###### Adding role to User after creating role in Role DocType
 		user_doc = frappe.get_doc("User",self.emp_mail_id)
 		user_doc.append("roles",{"role":self.designation})
