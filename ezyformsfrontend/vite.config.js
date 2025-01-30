f9f21eb4212cbbbe0abbe98248b36eb5d8bb28de
Sai kumar chittimalla <sai.chittimalla19@gmail.com>
2025-01-30 18:42:26 +0530

    save draft build file

--- ezyformsfrontend/vite.config.js
+++ ezyformsfrontend/vite.config.js
@@ -25,7 +25,7 @@
 // });
 
 
-// //"IN THE PODUCTION MODE NEED TO UNCOMMENT THE BELOW CODE" 
+// //"IN THE PODUCTION MODE NEED TO UNCOMMENT THE BELOW CODE"  
 
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
