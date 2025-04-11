3036f67befa7eacdcf06514cc847f427bfa3ead1
caratred <bharathkumar@caratred.com>
2025-04-11 19:11:06 +0530

    Create email Approval Doctype and custom api by girish

---
+++ ezy_forms/ezy_forms/doctype/email_approval/custom_email_approval.py
@@ -0,0 +1,79 @@
+from ezy_flow.ezy_flow.doctype.wf_workflow_requests.wf_workflow_requests import (
+    updating_wf_workflow_requests,wf_cancelling_request
+)
+import frappe
+from frappe.utils import add_to_date,now_datetime
+
+
+@frappe.whitelist(allow_guest=True)
+def email_approval(token, action=None, reason=None):
+    try:
+        email_doc = frappe.get_doc("Email Approval", {"action_token": token})
+
+        if email_doc.token_status == "Inactive":
+            return {"success":False,"message":"Invalid or expired token"}
+
+        # Get document detail
+        if not action:
+            get_doc = frappe.get_doc(
+                email_doc.doctype_name, {"name": email_doc.document_id}
+            )
+            approval_detail = frappe.get_doc("Ezy Employee",{"name":email_doc.user_id},["emp_name","signature"])
+           
+            if int(email_doc.current_level) == 1:
+                frappe.log_error(email_doc.current_level)
+                get_doc.approver = approval_detail.emp_name
+                get_doc.approved_by = approval_detail.signature
+                get_doc.approved_on = now_datetime().replace(second=0, microsecond=0)
+                get_doc.save(ignore_permissions=True)
+                frappe.db.commit()
+                get_doc.reload()
+            else:
+                setattr(get_doc, f"approver_{int(email_doc.current_level)-1}", approval_detail.emp_name)
+                setattr(get_doc, f"approved_by_{int(email_doc.current_level)-1}", approval_detail.signature)
+                setattr(get_doc, f"approved_on_{int(email_doc.current_level)-1}",now_datetime().replace(second=0, microsecond=0))
+                get_doc.save(ignore_permissions=True)
+                frappe.db.commit()
+                get_doc.reload()
+
+                
+            data = get_doc.as_dict()
+            
+            get_wf_request = frappe.get_doc("WF Workflow Requests",{"name":email_doc.wf_request_id})
+            
+            wf_data = get_wf_request.as_dict()
+           
+            return {"success":True,"message":{"doc_data":data,"wf_data":wf_data}}
+
+        # Update Workflow Request.
+        request_details = [
+            {
+                "doctype": email_doc.doctype_name,
+                "request_ids": [email_doc.wf_request_id],
+                "current_level": email_doc.current_level,
+                "action": action,
+                "reason": reason,
+                "url_for_approval_id": None,
+                "files": [],
+                "property": email_doc.property,
+                "cluster_name": None,
+            }
+        ]
+        if action == "Approve":
+            get_return = updating_wf_workflow_requests(request_details, email_doc.user_id)
+            
+        if action == "Request Cancelled":
+            get_return = wf_cancelling_request(email_doc.doctype_name,email_doc.wf_request_id,email_doc.current_level,reason,files=[],property=email_doc.property,cluster_name=None,action=action,url_for_cancelling_id=None,user_session=email_doc.user_id)
+
+        
+        frappe.db.set_value(
+            "Email Approval",
+            {"action_token": token, "token_status": "Active"},
+            {"token_status": "Inactive"},
+        )
+        frappe.db.commit()
+
+        return {"success":True,"message":f"Form {get_return.get('message')} successfully"}
+
+    except Exception as e:
+        frappe.log_error(f"Email Approval Action Error: {str(e)}")
---
+++ ezy_forms/ezy_forms/doctype/email_approval/email_approval.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2025, bharath and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+import uuid
+
+
+class EmailApproval(Document):
+	pass
+
+
+
+def create_email_approval_records(
+    wf_request_id,
+    doctype_name,
+    document_id,
+    user_id,
+    wf_statsu,
+    current_level,
+    propert_id,
+    department=None,
+):
+    action_token = str(uuid.uuid4())
+
+    get_role_uses = frappe.db.get_list(
+        "WF Users",
+        {"parent": propert_id, "mail": user_id},
+        ["role_name", "mail"],
+        ignore_permissions=True,
+    )
+
+    # for each in get_role_uses:
+    new_doc = frappe.get_doc(
+        {
+            "doctype": "Email Approval",
+            "property": propert_id,
+            "wf_request_id": wf_request_id,
+            "doctype_name": doctype_name,
+            "document_id": document_id[0].name,
+            "role": (
+                get_role_uses[0].get("role_name") if len(get_role_uses) > 0 else None
+            ),
+            "user_id": user_id,
+            "action_token": action_token,
+            "status": wf_statsu,
+            "department": department,
+            "current_level": current_level,
+        }
+    )
+
+    new_doc.insert(ignore_permissions=True)
+    frappe.db.commit()
+
+    return new_doc.action_token
+
+
+
