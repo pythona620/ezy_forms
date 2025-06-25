c7bbedd4177ce96b32ffe9a92deea0a227ae34a1
interns <interns@caratred.com>
2025-06-25 10:15:31 +0530

    modal popup showing when raise request

--- ezyformsfrontend/src/Pages/Settings/EmployeeComp.vue
+++ ezyformsfrontend/src/Pages/Settings/EmployeeComp.vue
@@ -14,11 +14,11 @@
             </p> -->
         </div>
         <div class="d-flex align-items-center gap-2">
-           <button type="button" class=" btn btn-light  CreateDepartments font-12" data-bs-toggle="modal" data-bs-target="#ExportEmployeeModal">
+           <button type="button" class=" btn export-btn  CreateDepartments font-12" data-bs-toggle="modal" data-bs-target="#ExportEmployeeModal">
             Export Employees
           </button>
 
-          <button type="button" class=" btn btn-light  CreateDepartments  font-12 " @click="bulkEmp">
+          <button type="button" class=" btn export-btn  CreateDepartments  font-12 " @click="bulkEmp">
             Import Employees
           </button>
           
@@ -1425,6 +1425,18 @@ function confirmEmployeeToggle() {
   const isEnabled = selectedEmpRow.value.enable === '1' || selectedEmpRow.value.enable === 1;
   selectedEmpRow.value.enable = isEnabled ? 0 : 1;
 
+  if(selectedEmpRow.value.enable==0){
+    // Get current date and time in "YYYY-MM-DD HH:mm:ss" format
+  const now = new Date();
+  const pad = (n) => n.toString().padStart(2, '0');
+  const currentDateTime = `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())} ${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`;
+
+  // Add current_date to the payload
+  selectedEmpRow.value.enable_on = currentDateTime;
+
+  console.log("selectedEmpRow.value",selectedEmpRow.value.enable_on);
+  }
+
   axiosInstance
     .put(`${apis.resource}${doctypes.EzyEmployeeList}/${selectedEmpRow.value.name}`, selectedEmpRow.value)
     .then(() => {
@@ -1494,15 +1506,21 @@ const exportEmployeesToExcel = async () => {
 
     if (response.data) {
       const employees = response.data
-          const modal = bootstrap.Modal.getInstance(
+      if(employees.length){
+        const modal = bootstrap.Modal.getInstance(
           document.getElementById("ExportEmployeeModal")
           );
           modal.hide();
           toast.success("Successfully Completed")
-      const worksheet = XLSX.utils.json_to_sheet(employees)
-      const workbook = XLSX.utils.book_new()
-      XLSX.utils.book_append_sheet(workbook, worksheet, 'Employees')
-      XLSX.writeFile(workbook, 'EmployeeDetails.xlsx')
+
+        const worksheet = XLSX.utils.json_to_sheet(employees)
+        const workbook = XLSX.utils.book_new()
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Employees')
+        XLSX.writeFile(workbook, 'EmployeeDetails.xlsx')
+      }
+      else{
+        toast.error("No Employee Details")
+      }
     }
   } catch (error) {
     console.error('Error exporting employee data:', error)
@@ -1923,6 +1941,10 @@ function SaveEditEmp() {
   font-size: var(--eleven);
 
   // height: 32px;
+}
+
+.export-btn{
+  background-color: #99999961;
 }
 
 .cancelfilter {
