09116902068caebdb4db998b64be0b1ea099f659
Surya <surya@caratred.com>
2025-08-22 14:37:36 +0530

    While creating a Child Table Name, cols a Forbidden Error.

--- ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.js
+++ ezy_forms/ezy_forms/doctype/ezy_employee/ezy_employee.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2024, bharath and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ezy Employee", {
-// 	refresh(frm) {
+frappe.ui.form.on("Ezy Employee", {
+    refresh: function(frm) {
+        frm.fields_dict["responsible_units"].get_query = function(doc, cdt, cdn) {
+            return {
+                filters: [
+                    ["name", "!=", frm.doc.company_field]  // Exclude company_field value
+                ]
+            };
+        };
+    },
 
-// 	},
-// });
+    company_field: function(frm) {
+        // Refresh responsible_units when company changes
+        frm.refresh_field("responsible_units");
+    }
+});
