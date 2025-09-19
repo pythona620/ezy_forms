5f5ec1661b350be9be3cafe2589a76406f861b87
prasad <prasad.prasad@y>
2025-09-19 18:13:27 +0530

    Removed unnessary doctypes

--- ezy_forms/ezy_forms/doctype/xhtecdry/xhtecdry.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, bharath and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class xhtecdry(Document):
-	pass
